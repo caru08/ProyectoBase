da111d1c3c074ab27b26e2c5817a9b6bb469caac
German Schmidt <gerschmidtt@gmail.com>
2018-04-07 19:41:49 -0300

    secundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginViewComponent } from './login-view/login-view.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginViewComponent
   ],
   imports: [
     BrowserModule
